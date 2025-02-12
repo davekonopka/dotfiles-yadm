b339b4b527925462e21589f8eb58f8dd958c4a5b
Dave Konopka <dave.konopka@gmail.com>
2025-02-12 08:05:40 -0500

    feat: add homebrew helpers

--- .config/bash/autoload/homebrew.sh
+++ .config/bash/autoload/homebrew.sh
@@ -1,2 +1,16 @@
+function brew-update() {
+  brew update
+  brew upgrade
+  brew cleanup
+}
+
+function brew-bundle() {
+  brew bundle --file=~/Brewfile.$(hostname)
+}
+
+function brew-bundle-dump() {
+  brew bundle dump --file=~/Brewfile.$(hostname) --force
+}
+
 eval "$(/opt/homebrew/bin/brew shellenv)"
 export PATH="/usr/local/opt/grep/libexec/gnubin:/usr/local/sbin:$PATH"
