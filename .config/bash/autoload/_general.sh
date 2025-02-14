2fd94262077b0f2210e2eee9ce8aa4675d62b6bb
Dave Konopka <dave.konopka@gametime.co>
2025-02-14 10:51:43 -0500

    feat(bash): set XDG_CONFIG_HOME

--- .config/bash/autoload/_general.sh
+++ .config/bash/autoload/_general.sh
@@ -5,6 +5,7 @@ export HISTTIMEFORMAT='%h %d %H:%M:%S '
 export HISTCONTROL=ignoreboth
 export PROMPT_COMMAND="${PROMPT_COMMAND} history -a;"
 export EDITOR="nvim"
+export XDG_CONFIG_HOME="$HOME/.config"
 
 export PATH="$HOME/.local/bin:$PATH"
 
