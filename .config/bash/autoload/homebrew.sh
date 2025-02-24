9ebd0c6f220241ca32df77f35fc883ecd64d8bcf
Dave Konopka <dave.konopka@gametime.co>
2025-02-24 14:04:36 -0500

    chore(bash): update Brewfile with new hostname

--- .config/bash/autoload/homebrew.sh
+++ .config/bash/autoload/homebrew.sh
@@ -5,11 +5,11 @@ function brew-update() {
 }
 
 function brew-bundle() {
-  brew bundle --file=~/Brewfile.$(hostname)
+  brew bundle --file=~/Brewfile.$(hostname -s)
 }
 
 function brew-bundle-dump() {
-  brew bundle dump --file=~/Brewfile.$(hostname) --force
+  brew bundle dump --file=~/Brewfile.$(hostname -s) --force
 }
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
